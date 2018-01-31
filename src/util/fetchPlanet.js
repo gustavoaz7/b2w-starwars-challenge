689d6aa14f725df063d673f158c4217aae74039e
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-01-31 10:10:59 -0200

    Fix fetching repeated planets

--- src/util/fetchPlanet.js
+++ src/util/fetchPlanet.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const N_PLANETS = 61
+let indexesArray = [...Array(N_PLANETS + 1).keys()]
 
 export default function getPlanetInfo() {
-  const url = generateURL(generateRandomIndex())
+  const url = generateURL(generateNewIndex())
   return axios.get(url)
 }
 
@@ -10,6 +12,15 @@ function generateURL(n) {
   return `https://swapi.co/api/planets/${n}/`
 }
 
-function generateRandomIndex() {
-  return Math.floor(Math.random() * 61) + 1
+function generateNewIndex() {
+  let index = Math.floor(Math.random() * N_PLANETS) + 1
+  if (indexesArray.includes(index)) {
+    indexesArray[index] = 0
+    if (indexesArray.reduce((a,b) => a + b, 0) === 0) {
+      indexesArray = [...Array(N_PLANETS + 1).keys()]
+    }
+    return index
+  } else {
+    return generateNewIndex()
+  }
 }
